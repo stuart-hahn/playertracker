59f3f9d0a3fb18825980a281464960518882dc0e
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-08-14 13:47:27 -0400

    delete boilerplate from create-react-app and add initial basic functionality to list players

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = (props) => {
+  const [players, setPlayers] = useState(props.players)
+  const [newPlayer, setNewPlayer] = useState('')
+
+  const addPlayer = event => {
+    event.preventDefault()
+    const playerObject = {
+      id: players.length + 1,
+      name: newPlayer,
+      url: `twitch.tv/${newPlayer}`
+    }
+
+    setPlayers(players.concat(playerObject))
+    setNewPlayer('')
+  }
+
+  const handlePlayerChange = (event) => setNewPlayer(event.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Players</h1>
+      <ul>
+        {players.map(player => <li key={player.id}>{player.name}</li>)}
+      </ul>
+      <form onSubmit={addPlayer}>
+        <input value={newPlayer} placeholder="create new player" onChange={handlePlayerChange} />
+        <button type="submit">Add Player</button>
+      </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const players = [
+  { id: 1, name: "slump", url: "twitch.tv/slumpcity" },
+  { id: 2, name: "antcap24", url: "twitch.tv/antcap24" },
+]
+
+ReactDOM.render(<App players={players} />, document.getElementById('root'))
